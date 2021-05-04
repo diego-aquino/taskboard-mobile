8f183aab85af1651752acc57eee05aa31d4ae61e
Diego Aquino <diego.cruz001@gmail.com>
2021-05-04 16:35:46 -0300

    Initialize React Native / Expo project

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import styles from '~/styles/AppStyles';
+
+const App = () => (
+  <SafeAreaView style={styles.container}>Taskboard</SafeAreaView>
+);
+
+export default App;
