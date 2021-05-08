828a8fdddd09fb8ec28b6b08dcde5547a60e2933
Diego Aquino <diego.cruz001@gmail.com>
2021-05-08 13:56:50 -0300

    Move status bar style to individual pages

--- App.jsx
+++ App.jsx
@@ -6,7 +6,6 @@ import { AuthContextProvider } from '~/contexts/AuthContext';
 import { useFonts } from '~/hooks';
 import AppRoutes from '~/routes';
 import { Container } from '~/styles/AppStyles';
-import { StatusBar } from '~/styles/global';
 
 const App = () => {
   const [fontsAreLoaded] = useFonts();
@@ -17,7 +16,6 @@ const App = () => {
 
   return (
     <Container>
-      <StatusBar variant="light" />
       <AuthContextProvider>
         <AccountContextProvider>
           <AppRoutes />
