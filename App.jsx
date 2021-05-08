b9cace95573f3fac975bf7be23c2ba6e2d81482a
Diego Aquino <diego.cruz001@gmail.com>
2021-05-08 18:20:31 -0300

    Add styled components and migrate App styles

--- App.jsx
+++ App.jsx
@@ -1,13 +1,12 @@
 import AppLoading from 'expo-app-loading';
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
 
 import { AccountContextProvider } from '~/contexts/AccountContext';
 import { AuthContextProvider } from '~/contexts/AuthContext';
 import { useFonts } from '~/hooks';
 import AppRoutes from '~/routes';
-import styles from '~/styles/AppStyles';
-import variables from '~/styles/variables';
+import { Container } from '~/styles/AppStyles';
+import { StatusBar } from '~/styles/global';
 
 const App = () => {
   const [fontsAreLoaded] = useFonts();
@@ -17,17 +16,14 @@ const App = () => {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor={variables.colors.white}
-      />
+    <Container>
+      <StatusBar variant="light" />
       <AuthContextProvider>
         <AccountContextProvider>
           <AppRoutes />
         </AccountContextProvider>
       </AuthContextProvider>
-    </SafeAreaView>
+    </Container>
   );
 };
 
