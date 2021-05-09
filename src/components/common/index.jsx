d264c0b562ff20fd5a35759d699d6b44544af8fe
Diego Aquino <diego.cruz001@gmail.com>
2021-05-09 11:08:20 -0300

    Create animated sidebar component

--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -1,5 +1,6 @@
 import Alert from './Alert';
 import Button from './Button';
 import Input from './Input';
+import Sidebar from './Sidebar';
 
-export { Alert, Button, Input };
+export { Alert, Button, Input, Sidebar };
